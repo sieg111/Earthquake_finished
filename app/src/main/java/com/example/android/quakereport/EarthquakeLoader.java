d765ddc45f19f0ca026e005becd19d83c2e69d7a
sieg <m13760378141@163.com>
2016-10-12 11:55:27 +0800

    I am done

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.quakereport;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+/**
+ * Loads a list of earthquakes by using an AsyncTask to perform the
+ * network request to the given URL.
+ */
+public class EarthquakeLoader extends AsyncTaskLoader<List<EarthquakeFore>> {
+
+    /** Tag for log messages */
+    private static final String LOG_TAG = EarthquakeLoader.class.getName();
+
+    /** Query URL */
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link EarthquakeLoader}.
+     *
+     * @param context of the activity
+     * @param url to load data from
+     */
+    public EarthquakeLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    /**
+     * This is on a background thread.
+     */
+    @Override
+    public List<EarthquakeFore> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+        // 执行网络请求、解析响应和提取地震列表。
+        List<EarthquakeFore> earthquakes = QueryUtils.fetchEarthquakeData(mUrl);
+        return earthquakes;
+    }
+}
